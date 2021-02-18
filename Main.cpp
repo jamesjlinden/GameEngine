055cb2c21980193d06ca0fec5ca86020d08808da
James Linden <jamesjlinden@gmail.com>
2021-02-17 22:17:19 -0800

    Updating window title to reflect project name

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,3 @@
-/*
-
-	Copyright 2010 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Tutorial 01 - Create a window
-*/
-
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
@@ -60,7 +40,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutCreateWindow("Ogldev Tutorial");
 
     InitializeGlutCallbacks();
 
